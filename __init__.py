7bb98d1e00133336b228a0c7c348a8540f64b1da
Brian <BrianArchbold96@yahoo.ie>
2018-10-16 19:42:56 +0100

    mycroft-assignment: add except_response parameter which allows users to speak commands for assignment commands rather than only being able to type through the CLI.
    Signed-off-by: Brian <BrianArchbold96@yahoo.ie>

--- __init__.py
+++ __init__.py
@@ -24,7 +24,6 @@ class TemplateSkill(MycroftSkill):
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
         super(TemplateSkill, self).__init__(name="TemplateSkill")
-        print("Hello world")
         self.db = db_helper.db_helper(self.settings.get("student_id"))
 
     @intent_handler(IntentBuilder("").require("New_Assignment"))
@@ -36,7 +35,7 @@ class TemplateSkill(MycroftSkill):
     def handle_assignment_name(self, message):
         self._assignment_name = message.data.get("name")
         self.set_context("name_assignment", message.data.get("name"))
-        self.speak_dialog("assignment_due_date")
+        self.speak_dialog("assignment_due_date", expect_response=True)
 
     @intent_handler(IntentBuilder("").require("list_all"))
     def handle_list_assignment(self, message):
@@ -44,6 +43,8 @@ class TemplateSkill(MycroftSkill):
         if not assignment_list:
             self.speak_dialog("no_assignments")
             return
+
+        self.speak_dialog("will_list")
         for single_assignment in assignment_list:
             self.speak_dialog(single_assignment.name)
 
@@ -57,17 +58,15 @@ class TemplateSkill(MycroftSkill):
         self._due_date = self.due_date[0].strftime('%d/%m/%Y')
         if not self._due_date:
             self.speak_dialog("invalid_due_date")
-        print(self._due_date)
         self.set_context("due_date_assignment", self._due_date)
-        self.speak_dialog("assignment_module")
+        self.speak_dialog("assignment_module", expect_response=True)
 
     @intent_handler(IntentBuilder("").require("module").require("due_date_assignment"))
     def _handle_assignnment_module(self, message):
         module = message.data.get("module")
         self._module = message.data.get("module")
-        print(module)
         self.set_context("module_assignment", module)
-        self.speak_dialog("assignment_percentage")
+        self.speak_dialog("assignment_percentage", expect_response=True)
 
     @intent_handler(IntentBuilder("").require("percentage").require("module_assignment").
             optionally("percent"))
@@ -75,8 +74,7 @@ class TemplateSkill(MycroftSkill):
         #percentage = message.data.get("percentage")
         self._percentage = message.data.get("percentage")
         self.set_context("percentage_assignment", self._percentage)
-        self.speak_dialog("assignment_type")
-        print(self._percentage)
+        self.speak_dialog("assignment_type", expect_response=True)
 
     @intent_handler(IntentBuilder("").require("type").require("percentage_assignment"))
     def _handle_assignment_percentage(self, message):
@@ -89,23 +87,21 @@ class TemplateSkill(MycroftSkill):
         module_id = message.data.get("module_id")
         m_assignments = self.db.getAllModuleAssignments(module_id)
         if not m_assignments:
-            print("Nono")
+            self.speak_dialog("no_assignments")
             return
-
+        
         for assignment in m_assignments:
             self.speak_dialog(assignment.name)
 
     def _handle_push_assignment(self):
-        print("getting here")
         now = datetime.now()
         assigned_date = now.strftime('%d/%m/%Y')
         assignment_test = assignment.Assignment(assigned_date, self._module, self._due_date, self._percentage, 0, self._type, self._assignment_name) 
         self.db.pushAssignment(assignment_test)
-        print("pushing")
 
 
     def _make_assignment(self):
-        self.speak_dialog("assignment_name")
+        self.speak_dialog("assignment_name", expect_response=True)
         self.set_context("new_assignment", "new assignment")
 
 # The "create_skill()" method is used to create an instance of the skill.
